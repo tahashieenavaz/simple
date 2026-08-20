328ccf3c345c41bdcaaebd77e4cbdbe4c40f8087
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-20 11:43:40 +0200

    feat: moves infinite loop security check into its separate method

--- src/simple/template_engine.py
+++ src/simple/template_engine.py
@@ -1,6 +1,6 @@
 import re
 from typing import Optional
-from pathlib import Path
+from pathlib import Path, PosixPath
 from simple.mixins import PatternsMixin
 
 
@@ -9,26 +9,34 @@ class TemplateEngine(PatternsMixin):
         super().__init__()
         self.root = Path(root).resolve()
 
-    def check_target_path_relativity(self, *, path, filename: str):
+    def check_target_path_relativity(self, *, path: PosixPath, filename: str):
         if path.is_relative_to(self.root):
             return
 
         raise PermissionError(f"Access denied: {filename} is outside template root.")
 
-    def check_target_existence(self, *, path, filename: str):
+    def check_target_existence(self, *, path: PosixPath, filename: str):
         if path.is_file():
             return
 
         raise FileNotFoundError(f"Template not found: {filename}")
 
+    def check_visited_path_infinite_loop(
+        self, *, filename: str, path: PosixPath, visited_paths: set
+    ):
+        if path not in visited_paths:
+            return
+
+        raise RecursionError(f"Infinite inclusion loop detected: {filename}")
+
     def render(
         self,
         filename: str,
-        _visited_files: Optional[set] = None,
+        _visited_paths: Optional[set] = None,
         _context: Optional[dict] = {},
     ):
-        if _visited_files is None:
-            _visited_files = set()
+        if _visited_paths is None:
+            _visited_paths = set()
 
         if _context is None:
             _context = set()
@@ -37,15 +45,15 @@ class TemplateEngine(PatternsMixin):
 
         self.check_target_path_relativity(path=target_path, filename=filename)
         self.check_target_existence(path=target_path, filename=filename)
+        self.check_visited_path_infinite_loop(
+            path=target_path, filename=filename, visited_paths=_visited_paths.copy()
+        )
 
-        if target_path in _visited_files:
-            raise RecursionError(f"Infinite inclusion loop detected: {filename}")
-
-        _visited_files.add(target_path)
+        _visited_paths.add(target_path)
         content = target_path.read_text(encoding="utf-8")
 
         def _match_resolver(match) -> str:
             included_filename = match.group(1)
-            return self.render(included_filename, _visited_files.copy())
+            return self.render(included_filename, _visited_paths.copy())
 
         return self.include_pattern.sub(_match_resolver, content)
