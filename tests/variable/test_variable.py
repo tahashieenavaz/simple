f170de3151cc171e69fa40557f105a72025493cc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-20 23:50:49 +0200

    test: add test for rendering single variables in template engine

---
+++ tests/variable/test_variable.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from simple import TemplateEngine
+
+
+def test_single_variables_are_rendered():
+    root_path = Path(__file__).resolve().parent
+    templates_path = root_path / "templates" / "base"
+    engine = TemplateEngine(templates_path)
+    rendered = engine.render("index.html", {"name": "John Doe"})
+    assert "Name: John Doe" in rendered
