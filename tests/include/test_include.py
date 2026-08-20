256e03180efa50d7fd6c91a295dbe444ee92f9e9
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-20 10:25:55 +0200

    test: test multiple file inclusion capability

--- tests/include/test_include.py
+++ tests/include/test_include.py
@@ -8,3 +8,13 @@ def test_files_could_be_included():
     engine = TemplateEngine(templates_path)
     rendered = engine.render("index.html")
     assert "extra.html" in rendered
+
+
+def test_multiple_files_could_be_included():
+    root_path = Path(__file__).resolve().parent
+    templates_path = root_path / "templates"
+    engine = TemplateEngine(templates_path)
+    rendered = engine.render("index-multiple.html")
+
+    assert "this is coming from part-1.html file" in rendered
+    assert "this is coming from part-2.html file" in rendered
