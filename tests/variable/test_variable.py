0a0f1ab8b79695ec458c63a38c341ed66d073b98
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-20 23:55:36 +0200

    test: adds multiple variable test support

--- tests/variable/test_variable.py
+++ tests/variable/test_variable.py
@@ -8,3 +8,13 @@ def test_single_variables_are_rendered():
     engine = TemplateEngine(templates_path)
     rendered = engine.render("single-variable.html", {"name": "John Doe"})
     assert "Name: John Doe" in rendered
+
+
+def test_multiple_variables_are_rendered():
+    root_path = Path(__file__).resolve().parent
+    templates_path = root_path / "templates"
+    engine = TemplateEngine(templates_path)
+    rendered = engine.render(
+        "multiple-variable.html", {"name": "John Doe", "prefix": "Mr. "}
+    )
+    assert "Name: Mr. John Doe" in rendered
